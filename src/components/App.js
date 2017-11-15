5eb751cb6dd659d7dd034025b1c5af5c560d9b36
Nam <namvic3000@gmail.com>
2017-11-16 00:19:44 +0800

    added react jsx to src/index to prove react now works on app

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+class App extends React.Component {
+    state = {}
+
+    render() {
+        return (
+            <div>
+                <h1>THIS IS APP, A FULL STATE COMPONENT, FROM EXTERNAL FILE</h1>
+            </div>
+        )
+    }
+}
+
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+
+  import React from 'react'
+  import ReactDOM from 'react-dom'
+  import App from './components/App.js'
+
+
+
+
 //if we want this page to render a App component into root component
 //of the views/index.ejs page, we create the react compnent and do
 //ReactDom.render here on this page.
@@ -18,14 +26,39 @@
 //views/index.ejs page. We could render a react component instead, if
 //that's what we wanted. if we do that, we need to import the 
 //react modules, and compose a react component, e.g <App/>
+
+
 let root = document.getElementById('root')
 
-root.innerHTML =
- `this is NEW10 text set by src/index.js, replacing old text in
-  index.ejs. This is src/index.js file, but now is public/bundle.js file
-  and public/bundle.js file is now linked to views/index.ejs
-   instead of the normal public/index.js which now has been moved to 
-  src/index.js because it is no longer linked to views/index.ejs so no
-  need for it to be in the /public folder. we can leave it in the /public
-  folder if we wanted to, but it is not linked to, it is only used as 
-  a source  to compile the public/bundle.js file`
+
+
+// root.innerHTML =
+//  `this is NEW11 text set by src/index.js, replacing old text in
+//   index.ejs. This is src/index.js file, but now is public/bundle.js file
+//   and public/bundle.js file is now linked to views/index.ejs
+//    instead of the normal public/index.js which now has been moved to 
+//   src/index.js because it is no longer linked to views/index.ejs so no
+//   need for it to be in the /public folder. we can leave it in the /public
+//   folder if we wanted to, but it is not linked to, it is only used as 
+//   a source  to compile the public/bundle.js file`
+
+
+
+
+  //now add react library, and create <App> component for render 
+  //into 'root' div of the views/index.ejs file
+
+
+  let Component1 = (props) => {
+      return(
+          <div>
+              <h1>THIS IS STATELESS COMPONENT1 WITHIN FILE</h1>
+          </div>
+      )
+  }
+
+
+
+  ReactDOM.render(<div><App/> <Component1/></div>, root  )
+
+ 
